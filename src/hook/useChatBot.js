17c0141ac6a5a99aafeed5bc0d1427f085bf7a7a
SubramanyaKS <subramanyaks2205@gmail.com>
2025-08-01 07:42:57 +0530

    removed vulnerability connected ai based chatbot

--- src/hook/useChatBot.js
+++ src/hook/useChatBot.js
@@ -1,47 +1,49 @@
 
-import { rules,responses } from "../utils/chatbot";
+import { rules, responses } from "../utils/chatbot";
 import { useState } from "react";
+import axios from "axios";
 
-export const useChatBot = ()=>{
-    const [messages, setMessages] = useState([
-        { sender: "bot", text: "Hello! How can I assist you today?" },
-      ]);
-      const [isChatbotVisible, setIsChatbotVisible] = useState(false);
-      const [userInput, setUserInput] = useState("");
-    
-      const botreply = ()=>{
-        const normalizedInput = userInput.toLowerCase();
-        var botReply = { sender: "bot", text: "I'm sorry, I didn't understand that."};
-        if(normalizedInput=='hi'|| normalizedInput=='hello'){
-            botReply = { sender: "bot", text: "Hi, How can I help you?"};
-        }
-        for (let category in rules) {
-          if (rules[category].some((keyword) => normalizedInput.includes(keyword))) {
-            botReply = { sender: "bot", text: responses[category] };
-            // botResponse = responses[category];
-            break;
-          }
-        }
-        
-    
-        setMessages((prevMessages) => [...prevMessages, botReply]);
-      }
-    
-      const handleSendMessage = () => {
-        if (userInput.trim() === "") return;
-    
-        const newMessages = [...messages, { sender: "user", text: userInput }];
-        setMessages(newMessages);
-    
-        botreply();
-    
-        setUserInput("");
-      };
-    
-      const handleKeyPress = (e) => {
-        if (e.key === "Enter") handleSendMessage();
-      };
+export const useChatBot = () => {
+  const [messages, setMessages] = useState([
+    { sender: "bot", text: "Hello! How can I assist you today?" },
+  ]);
+  const [isChatbotVisible, setIsChatbotVisible] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [loading,setLoading]=useState(false);
 
-      return{handleKeyPress,messages,isChatbotVisible,setIsChatbotVisible,userInput,handleSendMessage,setUserInput}
-    
+  const botreply = () => {
+
+    try {
+      const query = userInput.toLowerCase();
+      axios.post('https://subot-backend.onrender.com/ask', { query })
+        .then((res) => {
+          const botReply = { sender: "bot", text: res.data['response'] }
+          setLoading(!loading)
+          setMessages((prevMessage) => [...prevMessage, botReply]);
+        })
+        .catch((err) => console.log(err))
+    } catch (error) {
+      console.error(error);
+
+    }
+  }
+
+  const handleSendMessage = () => {
+    if (userInput.trim() === "") return;
+
+    const newMessages = [...messages, { sender: "user", text: userInput }];
+    setMessages(newMessages);
+    setLoading(!loading)
+
+    botreply();
+
+    setUserInput("");
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") handleSendMessage();
+  };
+
+  return { handleKeyPress, loading, messages, isChatbotVisible, setIsChatbotVisible, userInput, handleSendMessage, setUserInput }
+
 }
